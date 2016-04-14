fcbc71ce2b147e1e6ef78fa5c78f23a5df32cd49
gulyabanimen <gulyabanimen14@gmail.com>
2016-04-14 18:49:16 +0300

    Update WW PreAlpha PreBeta Bla Bla.lua

--- WW PreAlpha PreBeta Bla Bla.lua
+++ WW PreAlpha PreBeta Bla Bla.lua
@@ -2,52 +2,58 @@ if GetObjectName(GetMyHero()) ~= "Warwick" then return end
 
 require('Inspired')
 
-local manaQ = GetCastMana(myHero, _Q, GetCastLevel(myHero,_Q))
-local manaW = GetCastMana(myHero, _W, GetCastLevel(myHero,_W))
-local manaE = GetCastMana(myHero, _R, GetCastLevel(myHero,_R))
-
 local WWMenu = MenuConfig("Warwick","Warwick")
 
+WWMenu:Menu("Combo")
+
+WWMenu.Combo:Menu("QSettings", "Q - Settings")
+WWMenu.Combo.QSettings:Boolean("Q", "Use Q", true)
+WWMenu.Combo.QSettings:Slider("QMana", "Use Q if %mana >",70, 1, 100, 1) 
+
+
+WWMenu.Combo:Menu("WSettings", "W - Settings")
+WWMenu.Combo.WSettings:Boolean("W", "Use W", true)
+WWMenu.Combo.QSettings:Slider("QMana", "Use Q if %mana >",35, 1, 100, 1)
+
+
+WWMenu.Combo:Menu("RSettings", "R - Settings")
+WWMenu.Combo.RSettings:Boolean("R", "Use R", false)
+
+WWMenu:Menu("Killsteal", "Killsteal")
+WWMenu.Killsteal:Boolean("KillQ", "Use Q", true)
+WWMenu.Killsteal:Boolean("KillIgnite", "Use Ignite", false)
+
 WWMenu:Menu("Drawings", "Drawings")
-WWMenu:SubMenu("Combo", "Combo")
-WWMenu.Combo:Boolean("Q", "Use Q", true)
-WWMenu.Combo:Boolean("W", "Use W", true)
-WWMenu.Combo:Boolean("R", "Use R", true)
-WWkMenu.Drawings:Boolean("Q", "Draw Q Range", true)
-WWMenu.Drawings:Boolean("W", "Draw W Range", true)
-WWMenu.Drawings:Boolean("E", "Draw E Range", true)
+WWMenu.Drawings:Boolean("DrawQ", "Draw Q Range", true)
+WWMenu.Drawings:Boolean("DrawW", "Draw W&R Range", true)
+WWMenu.Drawings:Slider("Quality", "Quality", 125, 1, 255)
+
+local Ignite = (GetCastName(GetMyHero(),SUMMONER_1):lower():find("summonerdot") and SUMMONER_1 or (GetCastName(GetMyHero(),SUMMONER_2):lower():find("summonerdot") and SUMMONER_2 or nil))
 
 OnDraw(function(myHero)
-local pos = GetOrigin(myHero)
-if WWMenu.Drawings.Q:Value() then DrawCircle(pos,400,1,10,GoS.Red) end
-if WWMenu.Drawings.W:Value() then DrawCircle(pos,1250,1,10,GoS.Yellow) end
-if WWMenu.Drawings.W:Value() then DrawCircle(pos,600,1,10,GoS.Yellow) end
-if WWMenu.Drawings.R:Value() then DrawCircle(pos,700,1,10,GoS.Pink) end
+
+	if WWMenu.Drawings.DrawQ:Value() and IsReady(_Q) then
+		DrawCircle(GetOrigin(myHero), 400, 1, WWMenu.Drawings.Quality:Value(), GoS.Green)
+		--DrawCircle(Vector3D, radius, width, quality, color)
+	end
+	if WWMenu.Drawings.DrawW:Value() and (IsReady(_W) or IsReady(_R)) then
+		DrawCircle(GetOrigin(myHero), 900, 1, WWMenu.Drawings.Quality:Value(), GoS.Yellow)
+	end
 end)
 
 OnTick(function(myHero)
 
-	local target = GetCurrentTarget()
-	
-	if target ~= nil then
-	
-	if GetCurrentMana(myHero) > manaQ + manaW + manaE and IOW:Mode() == "Combo" then
-	
-	 if WWMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 400) then
-	 
-			CastSpell(target, _Q)
-		end
-	
-	if WWMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 600) and Ready(_Q) then
-	
-			CastSpell(_W)
-		end
-	end
-end
-end
-
-	
-
-	
-	
-	
+		local target = GetCurrentTarget()
+		
+		if IOW:Mode == "Combo" then
+			
+			if IsReady(_Q) and (GetDistance(target) <= 400 )then
+				CastSpell(_Q)
+			
+			
+					
+							
+							
+							
+							
+							
